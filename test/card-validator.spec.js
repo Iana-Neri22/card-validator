ad1b57270e2f7f1aacbc8be65f492d788739ad0b
Iana-Neri22 <iananeri@outlook.com>
2019-04-28 23:27:01 -0300

    checking number length

--- test/card-validator.spec.js
+++ test/card-validator.spec.js
@@ -5,7 +5,7 @@ let Card = require("../lib/card-validator")
 describe("card lib", () => {
   describe("cardValidator", () => {
     it("Should return true to 4539878902538066", () => {
-      expect(Card.cardValidator(4539878902538066)).to.equal(true);
+      expect(Card.cardValidator("4539878902538066")).to.equal(true);
     });
   });
 });
