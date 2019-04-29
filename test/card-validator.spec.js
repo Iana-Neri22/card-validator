fac8a0a6e94f54371efff659457f94de434687c7
Iana-Neri22 <iananeri@outlook.com>
2019-04-29 00:16:45 -0300

    dead code removed

--- test/card-validator.spec.js
+++ test/card-validator.spec.js
@@ -4,8 +4,11 @@ let Card = require("../lib/card-validator")
 
 describe("card lib", () => {
   describe("cardValidator", () => {
-    it("Should return true to 4539878902538066", () => {
+    it("Should return true to string 4539878902538066", () => {
       expect(Card.cardValidator("4539878902538066")).to.equal(true);
+    });
+    it("Should return true to number 4539878902538066", () => {
+      expect(Card.cardValidator(4539878902538066)).to.equal(true);
     });
   });
 });
