61a68e76408e505fd04290f3b81e4398bd2b5a5c
Iana-Neri22 <iananeri@outlook.com>
2019-04-30 00:29:00 -0300

    checking length refactored

--- test/card-validator.spec.js
+++ test/card-validator.spec.js
@@ -3,21 +3,19 @@ let expect = chai.expect;
 let Card = require('../lib/card-validator');
 
 describe('card lib', () => {
-  describe('checkLength', () => {
+  describe('cardValidator', () => {
     it('Return false to 123456', () => {
-      expect(Card.checkLength("123456")).to.equal(false);
+      expect(Card.cardValidator("123456")).to.equal(false);
     });
     it('Return false to number > 16', () => {
-      expect(Card.checkLength('533910046540306022')).to.equal(false);
+      expect(Card.cardValidator('533910046540306022')).to.equal(false);
     });
     it('Return false to empty string', () => {
-      expect(Card.checkLength('')).to.equal(false);
+      expect(Card.cardValidator('')).to.equal(false);
     });
     it('Return false to number < 16', () => {
-      expect(Card.checkLength('55914495801')).to.equal(false);
+      expect(Card.cardValidator('55914495801')).to.equal(false);
     });
-  })
-  describe('cardValidator', () => {
     it('Return true to string 4539878902538066', () => {
       expect(Card.cardValidator('4539878902538066')).to.equal(true);
     });
